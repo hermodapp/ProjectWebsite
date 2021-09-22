8661dd8e2b766e6932f5bca2eb339b03c6bd8c5f
Do Thien An Duong <root@DESKTOP-EOLPRDI.localdomain>
2021-09-21 19:15:04 -0500

    Modified README.md

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-This is a starter template for [Learn Next.js](https://nextjs.org/learn).
+# Hermod Project Website
+
+Hermod Project Website is created by create-next-app.
+
+## Website
+
+### Running API
+
+#### Installation Instructions for Mac/Linux
+
+```
+# Install node modules
+npm install
+
+# Build and run application
+npm run dev
+
+```
--- pages/about.js
+++ pages/about.js
@@ -1,19 +1,23 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import Header from "../components/header";
+import Footer from "../components/footer";
 
 export default function Home() {
   return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>Welcome to the Team</p>
-        <p>
-          This is a description
-        </p>
-      </section>
-    </Layout>
-  )
+    <>
+      <Header />
+      <Layout home>
+        <Head>
+          <title>{siteTitle}</title>
+        </Head>
+        <section className={utilStyles.headingMd}>
+          <p>Welcome to the Team</p>
+          <p>This is a description</p>
+        </section>
+      </Layout>
+      <Footer />
+    </>
+  );
 }
