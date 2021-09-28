b9c684def45b68b3e7c93d40d81616f3cc5d8436
Do Thien An Duong <root@DESKTOP-EOLPRDI.localdomain>
2021-09-28 13:12:26 -0500

    Add Rust API to deliverables

--- pages/deliverables.js
+++ pages/deliverables.js
@@ -140,6 +140,25 @@ export default function Deliverables() {
                   </span>
                   GitHub
                 </a>
+                <a
+                  href="https://hermodapp.github.io/rustdocs/hermodapi/"
+                  class="focus:outline-none hover:underline"
+                >
+                  <span class="bg-nord10 text-nord6 w-4 h-4 mr-2 rounded-full inline-flex items-center justify-center">
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="3"
+                      class="w-3 h-3"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M20 6L9 17l-5-5"></path>
+                    </svg>
+                  </span>
+                  Rust API Documentation
+                </a>
               </nav>
             </div>
           </div>
